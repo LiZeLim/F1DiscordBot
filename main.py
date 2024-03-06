276f290ae48d2c220a362d10f0a04938802cadd7
Richard Lim <lizelim995@gmail.com>
2024-03-07 00:44:17 +1100

    Fixed variable names, added histogram for Winners of the season

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 from datetime import date
 import pandas as pd
 from io import StringIO
+import matplotlib.pyplot as plt
 
-""" Formula 1 """
+""" Formula 1 Current Season Results"""
 
 # Reading from the site
 url_race_results = "https://www.formula1.com/en/results.html/2023/races.html"
@@ -15,9 +16,13 @@ soup = BeautifulSoup(html, features= "lxml")
 
 # finding the results table from the html webpage
 race_results = soup.find_all('table')[0]
-race_pd = pd.read_html(StringIO(str(race_results)), header=[0])[0]
-#print(list(race_pd.columns.values))
+race_df = pd.read_html(StringIO(str(race_results)), header=[0])[0]
+#print(list(race_df.columns.values))
 
-#droping useless columns
-race_pd = race_pd.drop(["Unnamed: 0", "Unnamed: 7"], axis = 1)
-#print(race_pd.head())
+#dropping useless columns
+race_df = race_df.drop(["Unnamed: 0", "Unnamed: 7"], axis = 1)
+print(race_df.info())
+
+value_counts = race_df['Winner'].value_counts()
+value_counts.plot(kind='bar')
+plt.show()
